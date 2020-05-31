4029cd987709ebb141e7b56c7646609a78a62337
Arturo A. Reyes Vital <arthurrevit@gmail.com>
2020-05-31 02:56:30 -0500

    Se desarrollaron componentes estáticos. Falta integración de info en la BD
    Signed-off-by: Arturo A. Reyes Vital <arthurrevit@gmail.com>

---
+++ client/src/components/common/buttons/SimpleBlackButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SimpleBlackButton = (props) => {
+  return <button className="simple-black-button">{props.text}</button>;
+};
+
+export default SimpleBlackButton;
